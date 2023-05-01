35ba04f7d2d10c840667963f6d136fea478932dd
NicoDaSkater <nicolas.y.nunezbaez@gmail.com>
2023-05-01 10:24:50 -0400

    made database tables structure except image column

---
+++ src/db/migrations/20230501002305_create_purchases_table.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => knex.schema.createTable('purchases', (table) => {
+    table.increments();
+    table.integer('price').notNullable();
+    table.integer('user_id').notNullable();
+    table.integer('listing_id').notNullable();
+    table.datetime('purchased_at', true)
+    });
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.schema.dropTable('purchases');
